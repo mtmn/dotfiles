74582b9152bb043362d456badfbacbc033ab1982
mtmn <miro@haravara.org>
2026-01-29 19:36:51 +0100

    chore: cleanup was long overdue

--- dot_config/nvim/lua/aliases.lua
+++ dot_config/nvim/lua/aliases.lua
@@ -1,6 +1,6 @@
-vim.cmd("cnoreabbrev nt botright vert new\n          cnoreabbrev spt %!saturn_play_tracks")
+vim.cmd("cnoreabbrev nt botright vert new\n          cnoreabbrev sct %!saturn_copy_tracks\n          cnoreabbrev spt %!saturn_play_tracks")
 local home = os.getenv("HOME")
-local oil_abbreviations = {mi = "/misc/", src = "/src/"}
+local oil_abbreviations = {bl = "/misc/music/backlog/", mi = "/misc/", mu = "/misc/music/", no = "/misc/notes/nota/", src = "/src/", rls = "/misc/notes/releases/", pls = "/.config/mpd/playlists", mtmn = "/src/codeberg.org/mtmn"}
 for abbrev, path in pairs(oil_abbreviations) do
   vim.cmd(("cnoreabbrev " .. abbrev .. " Oil " .. home .. path))
 end
--- dot_config/waybar/scripts/executable_network-status.sh
+++ dot_config/waybar/scripts/executable_network-status.sh
@@ -2,6 +2,4 @@
 
 if ip link show wg-njalla 2>/dev/null | grep -q "UP"; then
     nmcli device show wlp3s0 | awk '/GENERAL.CONNECTION:/ {print $2 " ✔"}'
-else
-    nmcli device show wlp3s0 | awk '/GENERAL.CONNECTION:/ {print $2 " 🗙"}'
 fi
